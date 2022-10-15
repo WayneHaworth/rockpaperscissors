f9d6c7334cadd7fbda522eeea3f73777683306d9
Wayne Haworth <waynehaworth@gmail.com>
2022-10-15 14:21:33 +0100

    Add logic

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 
-var rock = {
-    text: "Rock",
-    beats: scissors,
-    loses: paper
+var rock = { text: "rock", beats: 'scissors',loses: "paper" }
+var paper = { text: 'paper', beats: 'rock', loses: "scissors" }
+var scissors = { text: 'scissors', beats: 'paper', loses: "rock" }
+var selectionOptions = [rock, paper, scissors];
+var playerScore = 0;
+var computerScore = 0;
+
+function getChoice(arr) {
+    var choice = arr[Math.floor(Math.random() * arr.length)];
+    return choice.text;
 }
 
-const playerSelection = eval("scissors");
+function playSingleRound(playerSelection, computerSelectionString) {
+    var computerChoice = computerSelectionString;
+    playerSelection = playerSelection.toLowerCase();
 
-console.log(playerSelection);
+    if (playerSelection == computerChoice) {
+        return results = "Tie! You both picked " + playerSelection;
+    } else if (playerSelection == rock.text && computerChoice == rock.beats) {
+        playerScore++;
+        return results = "You win! Rock beats Scissors";
+    } else if (playerSelection == paper.text && computerChoice == paper.beats) {
+        playerScore++;
+        return results = "You win! Paper beats Rock";
+    } else if (playerSelection == scissors.text && computerChoice == scissors.beats) {
+        playerScore++;
+        return results = "You win! Scissors beats Paper";
+    } else {
+        computerScore++;
+        return results = "You lose! " + computerChoice + " beats " + playerSelection;
+    }
+}
 
+function game() {
+    for (let i=0; i<5; i++) {
+        var playerSelectionString = getChoice(selectionOptions)
+        playSingleRound(playerSelectionString, getChoice(selectionOptions))
+    }
+    
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+}
 
-
-
-
-// function getComputerChoice() {
-//     //randomly return either 'Rock', 'Paper' or 'Scissors'
-//     const options = ['Rock', 'Paper', 'Scissors'];
-//     return options[Math.floor(Math.random() * 3 )];
-// }
-
-// function playSingleRound(playerSelection, computerSelection) {
-//     const computerChoice = computerSelection();
-//     var result;
-//     if (playerSelection === "Rock") {
-//         if (computerChoice === "Rock") { return result = "DRAW" }
-//         else if ( computerChoice === "Paper") { return result = "COMPUTER WINS" }
-//         else if ( computerChoice === "Scissors") { return result = "PLAYER WINS" }
-//     }
-
-//     return result;
-// }
-
-// console.log(playSingleRound(playerSelection, getComputerChoice));
+game();

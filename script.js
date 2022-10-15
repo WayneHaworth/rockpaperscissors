9fd38738bec8df0926d12ef5bd64044d5e6e2d46
Wayne Haworth <waynehaworth@gmail.com>
2022-10-15 13:20:52 +0100

    refactor

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 
-
-
-const playerSelection = "Rock";
-
-function getComputerChoice() {
-    //randomly return either 'Rock', 'Paper' or 'Scissors'
-    const options = ['Rock', 'Paper', 'Scissors'];
-    return options[Math.floor(Math.random() * 3 )];
+var rock = {
+    text: "Rock",
+    beats: scissors,
+    loses: paper
 }
 
-function playSingleRound(playerSelection, computerSelection) {
-    const computerChoice = computerSelection();
-    var result;
-    if (playerSelection === "Rock") {
-        if (computerChoice === "Rock") { return result = "DRAW" }
-        else if ( computerChoice === "Paper") { return result = "COMPUTER WINS" }
-        else if ( computerChoice === "Scissors") { return result = "PLAYER WINS" }
-    }
+const playerSelection = eval("scissors");
 
-    return result;
-}
+console.log(playerSelection);
 
-console.log(playSingleRound(playerSelection, getComputerChoice));
+
+
+
+
+// function getComputerChoice() {
+//     //randomly return either 'Rock', 'Paper' or 'Scissors'
+//     const options = ['Rock', 'Paper', 'Scissors'];
+//     return options[Math.floor(Math.random() * 3 )];
+// }
+
+// function playSingleRound(playerSelection, computerSelection) {
+//     const computerChoice = computerSelection();
+//     var result;
+//     if (playerSelection === "Rock") {
+//         if (computerChoice === "Rock") { return result = "DRAW" }
+//         else if ( computerChoice === "Paper") { return result = "COMPUTER WINS" }
+//         else if ( computerChoice === "Scissors") { return result = "PLAYER WINS" }
+//     }
+
+//     return result;
+// }
+
+// console.log(playSingleRound(playerSelection, getComputerChoice));
